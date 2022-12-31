f2ebe84cfa95421631926240a9e38086d8a52982
belaymit <2belamit@gmail.com>
2022-12-31 10:16:58 +0300

    create module that handles global variables

---
+++ src/modules/constants.js
@@ -0,0 +1,4 @@
+export const movieDetailsPopup = document.querySelector('.popup-container');
+export const movieCloseBtn = document.getElementById('movie-close-btn');
+export const movieDetail = document.querySelector('.box-container');
+export const commentBtn = document.querySelector('#btn-comments');
